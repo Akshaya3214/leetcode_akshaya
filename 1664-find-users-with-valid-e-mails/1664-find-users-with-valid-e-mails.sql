c241814f699227b6caacbef3a5a47e76cd674f54
Akshaya3214 <22951a1227@iare.ac.in>
2025-03-21 22:45:24 +0530

    Time: 1091 ms (74.76%), Space: 0B (100%) - LeetHub

--- 1664-find-users-with-valid-e-mails/1664-find-users-with-valid-e-mails.sql
+++ 1664-find-users-with-valid-e-mails/1664-find-users-with-valid-e-mails.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
-/* select * from users 
-where mail REGEXP '^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode[.]com'; */
-
-# Write your MySQL query statement below
-select * from Users
+select * from users 
 where mail REGEXP '^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode[.]com$';
+
+/*
+select * from Users
+where mail REGEXP '^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode[.]com$'; */
