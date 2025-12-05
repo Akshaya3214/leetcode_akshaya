8954d6349d99cdcc3624851d01b625c0be4623a1
AKSHAYA REDDY KALLEPU <22951a1227@iare.ac.in>
2025-12-05 22:54:29 +0530

    Time: 2 ms (99.13%), Space: 47 MB (23.58%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,15 +1,15 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> hs = new HashMap<>();
+        HashMap <Integer, Integer> hs = new HashMap<>();
         for(int i=0; i<nums.length; i++){
-            int complement=target-nums[i];
+            int complement = target-nums[i];
 
             if(hs.containsKey(complement)){
-                return new int[]{hs.get(complement), i};
+                return new int[]{i, hs.get(complement)};
             }
-
             hs.put(nums[i], i);
         }
         return new int[]{-1, -1};
     }
 }
+
