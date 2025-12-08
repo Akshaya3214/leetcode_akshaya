30dc00607d55d3d384837b80c8342df1ea4c9d12
AKSHAYA REDDY KALLEPU <22951a1227@iare.ac.in>
2025-12-08 21:55:10 +0530

    Time: 1 ms (99.88%), Space: 94.7 MB (13.38%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,14 +1,15 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int n=prices.length;
-        int minprice=Integer.MAX_VALUE;
         int maxprofit=0;
+        int minprice=prices[0];
 
-        for(int price : prices){
-            if(price<minprice){
+        for(int price: prices){
+            if(price < minprice){
                 minprice=price;
-            }else if(price-minprice> maxprofit){
-                maxprofit=price-minprice;
+            }
+            else{
+                int currentprofit=price-minprice;
+                maxprofit=Math.max(price-minprice, maxprofit);
             }
         }
         return maxprofit;
