ef4b04fcf6cd4cab54c3dd4ada3bbd158ddb4caa
AKSHAYA REDDY KALLEPU <22951a1227@iare.ac.in>
2026-09-02 19:19:47 +0530

    Time: 9 ms (41.79%), Space: 45.5 MB (79.27%) - LeetHub

---
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] frequencySort(int[] nums) {
+        HashMap<Integer, Integer> hs = new HashMap<>();
+        Integer[] newNums = new Integer[nums.length];
+
+        for(int i=0; i<nums.length; i++){
+            hs.put(nums[i], hs.getOrDefault(nums[i], 0)+1);
+            newNums[i] = nums[i];
+        }
+        Arrays.sort(newNums, (n1, n2) -> {
+            if(hs.get(n1)!= hs.get(n2)){
+                return hs.get(n1)-hs.get(n2);
+            }else{
+                return n2-n1;
+            }
+        });
+        for(int i=0; i<nums.length; i++){
+            nums[i] = newNums[i];
+        }
+        return nums;
+        
+    }
+}
